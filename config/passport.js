d19b92494bf15d153acc372159e15afc54606f91
Your Name <kingrohi37@gmail.com>
2025-06-12 02:04:39 +0530

    Guard GoogleStrategy when credentials are missing

--- config/passport.js
+++ config/passport.js
@@ -4,47 +4,51 @@ const User = require('../models/User');
 const { v4: uuidv4 } = require('uuid');
 
 module.exports = function (passport) {
-  passport.use(
-    new GoogleStrategy(
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/api/auth/google/callback',
-      },
-      async (accessToken, refreshToken, profile, done) => {
-        const newUser = {
-          googleId: profile.id,
-          name: profile.displayName,
-          email: profile.emails[0].value,
-        };
+  if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
+    passport.use(
+      new GoogleStrategy(
+        {
+          clientID: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          callbackURL: '/api/auth/google/callback',
+        },
+        async (accessToken, refreshToken, profile, done) => {
+          const newUser = {
+            googleId: profile.id,
+            name: profile.displayName,
+            email: profile.emails[0].value,
+          };
 
-        try {
-          let user = await User.findOne({ googleId: profile.id });
+          try {
+            let user = await User.findOne({ googleId: profile.id });
 
-          if (user) {
-            done(null, user);
-          } else {
-            // If user does not exist, check if the email is already in use
-            user = await User.findOne({ email: newUser.email });
             if (user) {
-                // If email exists, link the Google ID to the existing account
-                user.googleId = newUser.googleId;
-                await user.save();
-                done(null, user);
+              done(null, user);
             } else {
-                // Otherwise, create a new user
-                newUser.emergencyId = uuidv4(); // Assign a unique emergency ID
-                user = await User.create(newUser);
-                done(null, user);
+              // If user does not exist, check if the email is already in use
+              user = await User.findOne({ email: newUser.email });
+              if (user) {
+                  // If email exists, link the Google ID to the existing account
+                  user.googleId = newUser.googleId;
+                  await user.save();
+                  done(null, user);
+              } else {
+                  // Otherwise, create a new user
+                  newUser.emergencyId = uuidv4(); // Assign a unique emergency ID
+                  user = await User.create(newUser);
+                  done(null, user);
+              }
             }
+          } catch (err) {
+            console.error(err);
+            done(err, null);
           }
-        } catch (err) {
-          console.error(err);
-          done(err, null);
         }
-      }
-    )
-  );
+      )
+    );
+  } else {
+    console.log('Google OAuth not configured (GOOGLE_CLIENT_ID missing). Skipping GoogleStrategy.');
+  }
 
   passport.serializeUser((user, done) => {
     done(null, user.id);
