bbdb15d16dc6e4257bee141ce65fb85357ca5806
Your Name <kingrohi37@gmail.com>
2025-06-15 19:00:26 +0530

    updated html js

--- routes/summary.js
+++ routes/summary.js
@@ -1,9 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const auth = require('../middleware/auth');
-const summaryController = require('../controllers/summaryController');
 
-// POST /api/summary  -> generate medical summary from profile
-router.post('/', auth, summaryController.generateSummary);
-
-module.exports = router;
